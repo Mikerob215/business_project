4ce9416ac02ca829d681d6b25c00576857bf9d08
mike <mikerobinson1@gmail.com>
2015-10-11 18:44:39 -0400

    added jquery to orders page

--- public/js/orders.js
+++ public/js/orders.js
@@ -1,11 +1,50 @@
-function modify_qty(val) {
-    var qty = document.getElementById('qty').value;
-    var new_qty = parseInt(qty,10) + val;
-    
-    if (new_qty < 0) {
-        new_qty = 0;
-    }
-    
-    document.getElementById('qty').value = new_qty;
-    return new_qty;
+$(document).ready(function() {
+  console.log("sanity test");
+
+ //inc/dev qty1
+$('.up1').on('click', function(event) {
+  event.preventDefault();
+  console.log("sanity check");
+  currentValue = parseInt($('.qty1').val())
+  $('.qty1').val(currentValue + 1)
+});
+$('.down1').on('click', function(event) {
+  event.preventDefault();
+  console.log("sanity check");
+  if ($('.qty1').val() > 0) {
+  currentValue = parseInt($('.qty1').val())
+  $('.qty1').val(currentValue - 1)
 }
+
+});
+//inc/dev qty3
+$('.up2').on('click', function(event) {
+  event.preventDefault();
+  console.log("sanity check");
+  currentValue = parseInt($('.qty2').val())
+  $('.qty2').val(currentValue + 1)
+});
+$('.down2').on('click', function(event) {
+  event.preventDefault();
+  console.log("sanity check");
+  if ($('.qty2').val() > 0) {
+  currentValue = parseInt($('.qty2').val())
+  $('.qty2').val(currentValue - 1)
+}
+});
+//inc/dev qty3
+$('.up3').on('click', function(event) {
+  event.preventDefault();
+  console.log("sanity check");
+  currentValue = parseInt($('.qty3').val())
+  $('.qty3').val(currentValue + 1)
+});
+$('.down3').on('click', function(event) {
+  event.preventDefault();
+  console.log("sanity check");
+  if ($('.qty3').val() > 0) {
+  currentValue = parseInt($('.qty3').val())
+  $('.qty3').val(currentValue - 1)
+}
+});
+});
