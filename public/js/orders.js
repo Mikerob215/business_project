df4362b67d0dc09d50716d6218b3560ea90194dc
mike <mikerobinson1@gmail.com>
2015-10-12 13:42:28 -0400

    fixed jquery; resized logo

--- public/js/orders.js
+++ public/js/orders.js
@@ -1,50 +1,19 @@
 $(document).ready(function() {
   console.log("sanity test");
-
- //inc/dev qty1
-$('.up1').on('click', function(event) {
-  event.preventDefault();
-  console.log("sanity check");
-  currentValue = parseInt($('.qty1').val())
-  $('.qty1').val(currentValue + 1)
-});
-$('.down1').on('click', function(event) {
-  event.preventDefault();
-  console.log("sanity check");
-  if ($('.qty1').val() > 0) {
-  currentValue = parseInt($('.qty1').val())
-  $('.qty1').val(currentValue - 1)
-}
-
-});
-//inc/dev qty3
-$('.up2').on('click', function(event) {
-  event.preventDefault();
-  console.log("sanity check");
-  currentValue = parseInt($('.qty2').val())
-  $('.qty2').val(currentValue + 1)
-});
-$('.down2').on('click', function(event) {
-  event.preventDefault();
-  console.log("sanity check");
-  if ($('.qty2').val() > 0) {
-  currentValue = parseInt($('.qty2').val())
-  $('.qty2').val(currentValue - 1)
-}
-});
-//inc/dev qty3
-$('.up3').on('click', function(event) {
-  event.preventDefault();
-  console.log("sanity check");
-  currentValue = parseInt($('.qty3').val())
-  $('.qty3').val(currentValue + 1)
-});
-$('.down3').on('click', function(event) {
-  event.preventDefault();
-  console.log("sanity check");
-  if ($('.qty3').val() > 0) {
-  currentValue = parseInt($('.qty3').val())
-  $('.qty3').val(currentValue - 1)
-}
-});
+// increase quantity
+  $('.up').on('click', function(event) {
+    event.preventDefault();
+    var currentValue = parseInt($(this).siblings('.qty').val());
+    if (currentValue >= 0) {
+      $(this).siblings('.qty').val(currentValue + 1)
+    };
+  });
+// decrease quantity
+    $('.down').on('click', function(event) {
+      event.preventDefault();
+      var currentValue = parseInt($(this).siblings('.qty').val());
+      if (currentValue >= 0) {
+        $(this).siblings('.qty').val(currentValue - 1)
+      };
+    });
 });
