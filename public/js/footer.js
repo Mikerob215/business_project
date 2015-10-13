ba3ec088ece3d53b355801c8bf437dfb92cc8312
Kevin Ferko <kevinferko@gmail.com>
2015-10-13 09:55:48 -0400

    design changes

--- public/js/footer.js
+++ public/js/footer.js
@@ -1,24 +1,49 @@
-var footbox_index = false;
-    var footbox_pos = 0;
-    var footbox_interval;
-    var framespeed = 5;
-function offer_mouseover() {
-    if (footbox_index == false) {
-        // clear previous interval
-        stop_function();
+// var footbox_index = false;
+//     var footbox_pos = 0;
+//     var footbox_interval;
+//     var framespeed = 5;
+// function offer_mouseover() {
+//     if (footbox_index == false) {
+//         // clear previous interval
+//         stop_function();
 
-        footbox_interval = setInterval(function () {
-            footbox_add();
-        }, 35);
-    }
-}
+//         footbox_interval = setInterval(function () {
+//             footbox_add();
+//         }, 35);
+//     }
+// }
 
-function footbox_mouseout() {
-    // clear previous interval
-    stop_function();
+// function footbox_mouseout() {
+//     // clear previous interval
+//     stop_function();
 
-    footbox_interval = setInterval(function () {
-        footbox_subtract();
-    }, 50);
-    console.log("mouseout");
-}
+//     footbox_interval = setInterval(function () {
+//         footbox_subtract();
+//     }, 50);
+//     console.log("mouseout");
+// }
+
+ $(".footbox").ready(function(){
+    var boxHeight = $(".footbox").height();
+        $(".footbox").mouseenter(function(){
+            $(this).animate({
+                height: "300px"
+            });
+        }).mouseleave(function(){
+            $(this).animate({
+                height: boxHeight
+            });
+        });
+    });
+// $(".footbox")
+//   .hover(function() {
+//     $(this).animate({ height: 250 }, 'fast');
+//   }, function() {
+//     $(this).animate({ height: 200 }, 'fast');
+//   });
+// $('.footbox').on('mouseover', function (event) {
+//     $('.footbox').css('max-height', '250px');        
+// });
+// $('.footbox').on('mouseout', function (event) {
+//     $('.footbox').css('max-height', '200px');
+// });
